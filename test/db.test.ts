3df4817bdb7688706e114de526a644222ffd5089
Hugo Josefson <hugo@josefson.org>
2023-05-14 17:26:34 +0200

    wip: tests pass

--- test/db.test.ts
+++ test/db.test.ts
@@ -1,4 +1,4 @@
-import { assertStrictEquals as eq } from "https://deno.land/std@0.187.0/testing/asserts.ts";
+import { assertEquals as eq } from "https://deno.land/std@0.187.0/testing/asserts.ts";
 import {
   beforeEach,
   describe,
