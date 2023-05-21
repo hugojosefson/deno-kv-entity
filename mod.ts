8c24f1f1e72b1f3d881c4137f347706b64743ca2
Hugo Josefson <hugo@josefson.org>
2023-05-21 19:04:56 +0200

    refactor: export things in mod.ts

---
+++ mod.ts
@@ -0,0 +1,10 @@
+export { EntityDb } from "./src/entity-db.ts";
+export type {
+  DbConfig,
+  DbConnectionCallback,
+  EntityDefinition,
+  EntityDefinitionId,
+  EntityInstance,
+  IndexedProperty,
+  PropertyLookupPair,
+} from "./src/types.ts";
